835c6ee12cd78cbd8caec77d11e8b074c6e5845e
Paul Buetow <paul@buetow.org>
2011-08-13 13:02:33 +0000

    added host based configs

--- index.pl
+++ index.pl
@@ -5,6 +5,12 @@ use warnings;
 
 use Xerl;
 
-my Xerl $xerl = Xerl->new( config => 'config.txt' );
+use Socket;
+use Sys::Hostname;
+
+my $host = hostname();
+my $config = -e "config-$host.txt" ?  "config-$host.txt" : 'config.txt';
+
+my Xerl $xerl = Xerl->new( config => $config );
 $xerl->run();
 
