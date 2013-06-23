5f1914c69df0e157cea184fb03a44202b4ea5265
Paul Buetow <paul@buetow.org>
2013-06-23 12:54:31 +0200

    remove

--- scifi.buetow.org/content/replace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-find . -name \*.inc | while read inc; do
-	sed -i -e 's/ü/\&uuml;/g; s/Ü/\&Uuml;/g; s/ö/\&ouml;/g; s/Ö/\&Ouml;/g; s/ä/\&auml;/g; s/Ä/\&Auml;/g; s/ß/\&szlig;/g; s/–/-/g;' $inc
-done
-
--- scifi.buetow.org/htdocs/images/convert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-for i in $(ls *.jpg | grep -v small | grep -v wallpaper); do 
-	convert -geometry 128 $i ${i/.jpg/-small.jpg}
-done
