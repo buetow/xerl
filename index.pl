fa6f017c3cd2fff8bb5f58445061ab84a7b80342
Paul Buetow (pluto.buetow.org) <paul@buetow.org>
2013-09-15 14:14:32 +0200

    xerldev.conf

--- index.pl
+++ index.pl
@@ -10,8 +10,8 @@ use Sys::Hostname;
 
 my $host = hostname();
 my $config =
-  -e "xerldev-$host.conf"
-  ? "xerldev-$host.conf"
+  -e "xerldev.conf"
+  ? "xerldev.conf"
   : ( -e "xerl-$host.conf" ? "xerl-$host.conf" : 'config.conf' );
 
 my Xerl $xerl = Xerl->new( config => $config );
