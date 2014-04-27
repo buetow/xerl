c04b203bfb37ae9dc665d42bbb270bda0293f762
Paul C. Buetow (mars.fritz.box) <paul@buetow.org>
2014-04-27 12:06:58 +0200

    You will forget that you ever knew me.

--- Xerl/Page/Content.pm
+++ Xerl/Page/Content.pm
@@ -111,7 +111,7 @@ sub _insertrules($$$) {
       elsif ( lc $name eq 'inject' ) {
         # Fetch via LWP::Simple
         my $got = get($text) or push @content "Error:$!";
-        $got = "$got:text:$text",
+        $got = "$got:text:$text";
         push @content, '<noop>', $got, '</noop>';
 
       }
